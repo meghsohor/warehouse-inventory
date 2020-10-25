5d6eaaabd0cb5fb05e18a1cc77bd941ec05439f6
Shuvo <meghsohor@yahoo.com>
2020-10-26 00:06:24 +0600

    tsDoc added in data-service

--- src/services/dataService.tsx
+++ src/services/dataService.tsx
@@ -12,6 +12,11 @@ localForage.config({
     ]
 });
 
+/**
+* Get all products from localStorage
+*
+* @returns products [] | []
+*/
 export async function getAllProducts() {
     return localForage.getItem(PRODUCT_KEY).then((products) => {
         if (products) return products;
@@ -19,6 +24,13 @@ export async function getAllProducts() {
     });
 }
 
+
+/**
+* Get single product from localStorage
+*
+* @param id: string
+* @returns product {} | null
+*/
 export async function getProduct(id:string) {
     return localForage.getItem(PRODUCT_KEY).then((products:any) => {
         if (products) {
@@ -28,6 +40,16 @@ export async function getProduct(id:string) {
     });
 }
 
+/**
+* Add a product to localStorage
+*
+* @remarks
+* Add product to the product list array
+* If there is no exisiting product list, creates an empty array
+*
+* @param product: {IProduct}
+* @returns true | false
+*/
 export async function addProduct(product:IProduct) {
     return localForage.getItem(PRODUCT_KEY).then((products: any) => {
         if (products) {
@@ -46,6 +68,12 @@ export async function addProduct(product:IProduct) {
     }); 
 }
 
+/**
+* Update multiple products
+*
+* @param products - an array of products to be updated
+* @returns All the products
+*/
 export async function updateProducts(products:IProduct[]) {
 
     return localForage.setItem(PRODUCT_KEY, products).then((data:any) => {
@@ -54,6 +82,19 @@ export async function updateProducts(products:IProduct[]) {
     });
 }
 
+
+/**
+* Update single product
+*
+* @remarks
+* Get all the products from the localStorage
+* find a product by the `product.id`
+* Create a new product list with the updated product
+* Store the new product list in the localStorage
+*
+* @param product {IProduct}
+* @returns products []
+*/
 export async function updateProduct(product: IProduct) {
     return localForage.getItem(PRODUCT_KEY).then((data) => {
         const products: any = data;
@@ -72,14 +113,27 @@ export async function updateProduct(product: IProduct) {
                 return item;
             });
 
-            return localForage.setItem(PRODUCT_KEY, newProducts).then((data: any) => {
-                if (data) return data;
-                return [];
+            return localForage.setItem(PRODUCT_KEY, newProducts).then((data:IProduct[]) => {
+                return data;
             });
         }
     });
 }
 
+/**
+* Delete product
+*
+* @remarks
+* Get all the products from the localStorage
+* find a product by the `product.id`
+* Create a new products array without the target product
+* Store the new product list in the localStorage
+* Delete the product's Price History
+* Delete the product's Quantity History
+*
+* @param id:string
+* @returns products []
+*/
 export async function deleteProduct(productID:string) {
     return localForage.getItem(PRODUCT_KEY).then((data) => {
         const products:any = data;
@@ -92,14 +146,34 @@ export async function deleteProduct(productID:string) {
             deleteProductQuantityHistory(productID);
 
 
-            return localForage.setItem(PRODUCT_KEY, newProducts).then((data: any) => {
-                if (data) return data;
+            return localForage.setItem(PRODUCT_KEY, newProducts).then((data) => {
+                if (data)
+                    return data;
                 return [];
             });
         }
     });
 }
 
+
+/**
+* Add/Update a product's price history
+* Product Price History is an object
+* Each product's id is a property
+* Each property contains an array of object
+* Each object contains the price and the time of price update
+* Price history array of each product can contain max 5 objects
+* 
+* @remarks
+* Get the price history of all the products
+* If the price history of the product exists, add the new price and current time as an item
+* If the price history of the products contains more than 5 items, delete the first/oldest item
+* If the price history of the product doesn't exist, create an empty array and add the new price and current time as an item
+*
+* @param id:string - id of the product
+* @param price:number - current price of the product
+* @returns void
+*/
 export async function setProductPriceHistory(id:string, price:number) {
     localForage.getItem(PRICE_HISTORY_KEY).then((res) => {
         let allProductsHistory:any = res;
@@ -131,9 +205,16 @@ export async function setProductPriceHistory(id:string, price:number) {
     });
 }
 
+
+/**
+* Get the price history of a product
+*
+* @param id: string
+* @returns history [] | null
+*/
 export async function getProductPriceHistory(id:string) {
     return localForage.getItem(PRICE_HISTORY_KEY).then((res) => {
-        let allProductsHistory: any = res;
+        let allProductsHistory:any = res;
         if (allProductsHistory && allProductsHistory[id] && allProductsHistory[id].priceHistory) {
             return allProductsHistory[id].priceHistory;
         } else {
@@ -142,19 +223,41 @@ export async function getProductPriceHistory(id:string) {
     });
 }
 
-/*-------------- Need to work on this ----------------------*/
+/**
+* Delete a product's price history
+* This function is called from `deleteProduct`
+*
+* @param id: string
+* @returns void
+*/
 export async function deleteProductPriceHistory(id: string) {
     localForage.getItem(PRICE_HISTORY_KEY).then((data) => {
         const priceHistory: any = data;
-        console.log(priceHistory[id])
         if (priceHistory && priceHistory[id]) {
-            const {id, ...newPriceHistory} = priceHistory;
-
-            localForage.setItem(PRICE_HISTORY_KEY, newPriceHistory);
+            delete priceHistory[id]
+            localForage.setItem(PRICE_HISTORY_KEY, priceHistory);
         }
     });
 }
 
+/**
+* Add/Update a product's quantity history
+* Product Quantity History is an object
+* Each product's id is a property
+* Each property contains an array of object
+* Each object contains the quantity and the time of quantity update
+* Quantity history array of each product can contain max 5 objects
+*
+* @remarks
+* Get the quantity history of all the products
+* If the quantity history of the product exists, add the new quantity and current time as an item
+* If the quantity history of the products contains more than 5 items, delete the first/oldest item
+* If the quantity history of the product doesn't exist, create an empty array and add the new quantity and current time as an item
+*
+* @param id:string - id of the product
+* @param quantity:number - current quantity of the product
+* @returns void
+*/
 export async function setProductQuantityHistory(id:string, quantity:number) {
     localForage.getItem(QUANTITY_HISTORY_KEY).then((res) => {
         let allProductsHistory: any = res;
@@ -185,6 +288,12 @@ export async function setProductQuantityHistory(id:string, quantity:number) {
     });
 }
 
+/**
+* Get the quantity history of a product
+*
+* @param id: string
+* @returns history [] | null
+*/
 export async function getProductQuantityHistory(id: string) {
     return localForage.getItem(QUANTITY_HISTORY_KEY).then((res) => {
         let allProductsHistory: any = res;
@@ -196,15 +305,21 @@ export async function getProductQuantityHistory(id: string) {
     });
 }
 
-/*-------------- Need to work on this ----------------------*/
+
+/**
+* Delete a product's quantity history
+* This function is called from `deleteProduct`
+*
+* @param id: string
+* @returns void
+*/
 export async function deleteProductQuantityHistory(id: string) {
     localForage.getItem(QUANTITY_HISTORY_KEY).then((data) => {
         const quantityHistory: any = data;
 
         if (quantityHistory && quantityHistory[id]) {
-            const { id, ...newQuantityHistory } = quantityHistory;
-
-            localForage.setItem(QUANTITY_HISTORY_KEY, newQuantityHistory);
+            delete quantityHistory[id]
+            localForage.setItem(QUANTITY_HISTORY_KEY, quantityHistory);
         }
     });
 }
