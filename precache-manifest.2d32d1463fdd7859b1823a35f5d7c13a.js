1954ea5d655d08e64a9fe21c567c1112261ad017
Shuvo <meghsohor@yahoo.com>
2020-11-29 21:06:40 +0600

    Updates

---
+++ precache-manifest.2d32d1463fdd7859b1823a35f5d7c13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a720a2a01a3ee2800ef05d7c6be7a321",
+    "url": "/warehouse-inventory/index.html"
+  },
+  {
+    "revision": "9794862bd11b0a8b3667",
+    "url": "/warehouse-inventory/static/js/2.cb20d29d.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/warehouse-inventory/static/js/2.cb20d29d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3211f573f495d1ba67a9",
+    "url": "/warehouse-inventory/static/js/main.0d708be1.chunk.js"
+  },
+  {
+    "revision": "e16cc283360c4674b5ff",
+    "url": "/warehouse-inventory/static/js/runtime-main.8ac487d8.js"
+  }
+]);
